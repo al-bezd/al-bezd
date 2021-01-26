19afe45c5a4957d3522e64d57d8648b7a8f62a4f
devops devops <devops999@yandex.ru>
2021-01-26 22:27:20 +0500

    fix bugs

--- AtackTitans/urls.py
+++ AtackTitans/urls.py
@@ -1,8 +1,11 @@
 from django.urls import include, path
 from .views import *
 from django.views.generic import TemplateView
+from .views import *
 urlpatterns = [
 
     path('', TemplateView.as_view(template_name='AtackTitans/index.html')),
+    path('getLinks/', getLinks),
+    path('getChapters/', getChapters),
 
 ]
--- AtackTitans/views.py
+++ AtackTitans/views.py
@@ -1,3 +1,25 @@
+import glob
+
+from django.http import JsonResponse
+import os
 from django.shortcuts import render
 
 # Create your views here.
+
+from wagtail.project_template.project_name.settings.base import BASE_DIR, PROJECT_DIR
+import importlib.resources
+
+
+def getLinks(request):
+    manga = request.GET['manga']
+    chapter = request.GET['chapter']
+    pages = glob.glob(f'{os.path.abspath(os.curdir)}\\AtackTitans\\static\\img\\{manga}\\{chapter}\\*.*')
+    pages = [f'/static{i.split("static")[1]}'.replace('\\','/') for i in pages]
+    return JsonResponse({'manga':manga,'chapter':chapter,'pages':pages})
+
+def getChapters(request):
+    manga = request.GET['manga']
+    #chapter = request.GET['chapter']
+    chapters = glob.glob(f'{os.path.abspath(os.curdir)}\\AtackTitans\\static\\img\\{manga}\\*')
+    chapters = [i.split('\\')[-1] for i in chapters]
+    return JsonResponse({'manga': manga, 'chapters': chapters})
