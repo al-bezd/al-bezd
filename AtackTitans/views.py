9f626d0f7dccfc843cad243338a95a7d4f5b84d4
Безденежных Александр <al.bezdenezhnykh@corp.santur.ru>
2021-01-27 15:28:59 +0500

    fix bugs

--- AtackTitans/views.py
+++ AtackTitans/views.py
@@ -11,11 +11,13 @@ def getLinks(request):
     path = path.replace('\\', '/')
     pages = glob.glob(path)
     pages = ['/static' + i.split("static")[1] + '' for i in pages]
+    pages = [i.replace('\\','/') for i in pages]
     if len(pages) == 0:
         path = "" + os.path.abspath(os.curdir) + "\\AtackTitans\\static\\img\\" + manga + "\\" + chapter + "\\*.*"
         path = path.replace('\\', '/')
         pages = glob.glob(path)
         pages = ['/static' + i.split("static")[1] + '' for i in pages]
+        pages = [i.replace('\\', '/') for i in pages]
 
     return JsonResponse({'manga': manga, 'chapter': chapter, 'pages': pages})
 
@@ -25,7 +27,8 @@ def getChapters(request):
     path = '' + os.path.abspath(os.curdir) + '\\static\\img\\' + manga + '\\*'
     path = path.replace('\\', '/')
     chapters = glob.glob(path)
-    chapters = [i.split('\\')[-1] for i in chapters]
+    chapters = [i.split('\\')[-1].replace('\\', '/') for i in chapters]
+    #chapters = [i.split('\\')[-1] for i in chapters]
     if len(chapters) == 0:
         path = '' + os.path.abspath(os.curdir) + '\\AtackTitans\\static\\img\\' + manga + '\\*'
         path = path.replace('\\', '/')
