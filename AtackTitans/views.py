2a3ec2348aedb4b5d8c1ae38f4a9439faabca641
devops devops <devops999@yandex.ru>
2021-01-27 20:01:39 +0500

    fix bugs

--- AtackTitans/views.py
+++ AtackTitans/views.py
@@ -1,39 +1,32 @@
 import glob
 from django.http import JsonResponse
 import os
+from mysite.settings.base import PROJECT_DIR, BASE_DIR
+from wagtail.project_template.project_name.settings.base import BASE_DIR
+
+
 
 
 def getChapters(request):
     manga = request.GET['manga']
-    path = '' + os.path.abspath(os.curdir).replace('mysite','AtackTitans') + '\\static\\img\\' + manga + '\\*'
+    path = '' + PROJECT_DIR.replace('mysite','AtackTitans') + '\\static\\img\\' + manga + '\\*'
     path = path.replace('\\', '/')
     chapters = glob.glob(path)
     chapters = [i.split('\\')[-1].replace('\\', '/') for i in chapters]
-    #chapters = [i.split('\\')[-1] for i in chapters]
-    if len(chapters) == 0:
-        path = '' + os.path.abspath(os.curdir) + '\\static\\img\\' + manga + '\\*'
-        path = path.replace('\\', '/')
-        chapters = glob.glob(path)
-        chapters = [i.split('\\')[-1].replace('\\', '/') for i in chapters]
-        #chapters = [i.replace('\\', '/') for i in chapters]
-    return JsonResponse({'manga': manga, 'chapters': chapters,'curdir':os.path.abspath(os.curdir).replace('mysite','AtackTitans')})
+
+    return JsonResponse({'manga': manga, 'chapters': chapters,'curdir':PROJECT_DIR.replace('mysite','AtackTitans')})
 
 
 
 def getLinks(request):
     manga = request.GET['manga']
     chapter = request.GET['chapter']
-    path = "" + os.path.abspath(os.curdir).replace('mysite','AtackTitans') + "\\static\\img\\" + manga + "\\" + chapter + "\\*.*"
+    path = "" + PROJECT_DIR.replace('mysite','AtackTitans') + "\\static\\img\\" + manga + "\\" + chapter + "\\*.*"
     path = path.replace('\\', '/')
     pages = glob.glob(path)
     pages = ['/static' + i.split("static")[1] + '' for i in pages]
     pages = [i.replace('\\','/') for i in pages]
-    if len(pages) == 0:
-        path = "" + os.path.abspath(os.curdir) + "\\static\\img\\" + manga + "\\" + chapter + "\\*.*"
-        path = path.replace('\\', '/')
-        pages = glob.glob(path)
-        pages = ['/static' + i.split("static")[1] + '' for i in pages]
-        pages = [i.replace('\\', '/') for i in pages]
+
 
     return JsonResponse({'manga': manga, 'chapter': chapter, 'pages': pages})
 
